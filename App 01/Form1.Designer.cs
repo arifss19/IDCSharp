2d6392dbd7a2aec1f7a0f0970f5656051ca091ba
Angga Adji Surya <anggas25@live.com>
2019-05-14 14:00:12 +0700

    update

---
+++ App 01/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace WinForm_APP
+{
+    partial class Warna
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnKeluar = new System.Windows.Forms.Button();
+            this.btnBeli = new System.Windows.Forms.Button();
+            this.Harga = new System.Windows.Forms.TextBox();
+            this.Item = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.itemList = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackColor = System.Drawing.Color.White;
+            this.splitContainer1.Panel1.Controls.Add(this.label7);
+            this.splitContainer1.Panel1.Controls.Add(this.label6);
+            this.splitContainer1.Panel1.Controls.Add(this.label5);
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.btnKeluar);
+            this.splitContainer1.Panel1.Controls.Add(this.btnBeli);
+            this.splitContainer1.Panel1.Controls.Add(this.Harga);
+            this.splitContainer1.Panel1.Controls.Add(this.Item);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(140)))), ((int)(((byte)(217)))));
+            this.splitContainer1.Panel2.Controls.Add(this.button1);
+            this.splitContainer1.Panel2.Controls.Add(this.label4);
+            this.splitContainer1.Panel2.Controls.Add(this.label2);
+            this.splitContainer1.Panel2.Controls.Add(this.itemList);
+            this.splitContainer1.Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.SplitContainer1_Panel2_Paint);
+            this.splitContainer1.Size = new System.Drawing.Size(463, 339);
+            this.splitContainer1.SplitterDistance = 206;
+            this.splitContainer1.TabIndex = 0;
+            this.splitContainer1.SplitterMoved += new System.Windows.Forms.SplitterEventHandler(this.SplitContainer1_SplitterMoved);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Black;
+            this.label7.Location = new System.Drawing.Point(22, 239);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 15);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Aksi";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(22, 136);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 15);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Item";
+            this.label6.Click += new System.EventHandler(this.Label6_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(22, 181);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 15);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Harga";
+            this.label5.Click += new System.EventHandler(this.Label5_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WinForm_APP.Properties.Resources.Photo_1554443974256;
+            this.pictureBox1.Location = new System.Drawing.Point(65, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(78, 90);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click_1);
+            // 
+            // btnKeluar
+            // 
+            this.btnKeluar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(140)))), ((int)(((byte)(217)))));
+            this.btnKeluar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnKeluar.Font = new System.Drawing.Font("Segoe UI Semibold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKeluar.ForeColor = System.Drawing.Color.White;
+            this.btnKeluar.Location = new System.Drawing.Point(25, 286);
+            this.btnKeluar.Name = "btnKeluar";
+            this.btnKeluar.Size = new System.Drawing.Size(162, 23);
+            this.btnKeluar.TabIndex = 3;
+            this.btnKeluar.Text = "Keluar";
+            this.btnKeluar.UseVisualStyleBackColor = false;
+            this.btnKeluar.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // btnBeli
+            // 
+            this.btnBeli.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(140)))), ((int)(((byte)(217)))));
+            this.btnBeli.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBeli.Font = new System.Drawing.Font("Segoe UI Semibold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBeli.ForeColor = System.Drawing.Color.White;
+            this.btnBeli.Location = new System.Drawing.Point(25, 257);
+            this.btnBeli.Name = "btnBeli";
+            this.btnBeli.Size = new System.Drawing.Size(162, 23);
+            this.btnBeli.TabIndex = 2;
+            this.btnBeli.Text = "Beli";
+            this.btnBeli.UseVisualStyleBackColor = false;
+            this.btnBeli.Click += new System.EventHandler(this.BtnBeli_Click);
+            // 
+            // Harga
+            // 
+            this.Harga.Enabled = false;
+            this.Harga.Location = new System.Drawing.Point(25, 199);
+            this.Harga.Name = "Harga";
+            this.Harga.Size = new System.Drawing.Size(156, 20);
+            this.Harga.TabIndex = 1;
+            this.Harga.TextChanged += new System.EventHandler(this.Harga_TextChanged);
+            // 
+            // Item
+            // 
+            this.Item.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Item.FormattingEnabled = true;
+            this.Item.Location = new System.Drawing.Point(25, 157);
+            this.Item.Name = "Item";
+            this.Item.Size = new System.Drawing.Size(156, 21);
+            this.Item.TabIndex = 0;
+            this.Item.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            this.Item.Click += new System.EventHandler(this.Item_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(140)))), ((int)(((byte)(217)))));
+            this.button1.Location = new System.Drawing.Point(166, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Bersihkan";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(13, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 15);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Item List";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(9, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Kasir";
+            this.label2.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // itemList
+            // 
+            this.itemList.FormattingEnabled = true;
+            this.itemList.Location = new System.Drawing.Point(16, 162);
+            this.itemList.Name = "itemList";
+            this.itemList.Size = new System.Drawing.Size(225, 147);
+            this.itemList.TabIndex = 0;
+            this.itemList.SelectedIndexChanged += new System.EventHandler(this.ListBox1_SelectedIndexChanged);
+            // 
+            // Warna
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(463, 339);
+            this.Controls.Add(this.splitContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Warna";
+            this.Text = "Kasir Sederhana";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ComboBox Item;
+        private System.Windows.Forms.TextBox Harga;
+        private System.Windows.Forms.Button btnKeluar;
+        private System.Windows.Forms.Button btnBeli;
+        private System.Windows.Forms.ListBox itemList;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ App 01/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForm_APP
+{
+    public partial class Warna : Form
+    {
+        public Warna()
+        {
+            InitializeComponent();
+            itemList.Items.Add("Pesanan " + " " + " Harga ");
+        }
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Item.Items.Add("Makaroni Pedas");
+            Item.Items.Add("Es Cincau");
+            Item.Items.Add("Mie Goreng");
+            Item.Items.Add("Mie Rebus");
+            Item.Items.Add("Telur");
+
+        }
+
+        private void SplitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void SplitContainer1_SplitterMoved(object sender, SplitterEventArgs e)
+        {
+
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (Item.Text == "Makaroni Pedas")
+            {
+                Harga.Text = "2000";
+            }
+            else if (Item.Text == "Es Cincau")
+            {
+                Harga.Text = "3000";
+            }
+            else if (Item.Text == "Mie Goreng")
+            {
+                Harga.Text = "6000";
+            }
+            else if (Item.Text == "Mie Rebus")
+            {
+                Harga.Text = "5000";
+            }
+            else if (Item.Text == "Telur")
+            {
+                Harga.Text = "1500";
+            }
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Kamu Yakin ?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+{
+                MessageBox.Show("Sampai Jumpa");
+                this.Close();
+            }
+            else
+            {
+   
+            }
+        }
+
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void PictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Item_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void BtnBeli_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(Harga.Text))
+            {
+                MessageBox.Show("Pilih Item Terlebih dahulu");
+            }
+            else
+                
+            itemList.Items.Add(Item.Text + " " + Harga.Text);
+            Harga.Clear();
+           
+        }
+
+         
+
+    private void Harga_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            itemList.Items.Clear();
+            itemList.Items.Add("Pesanan" + " " + " Harga ");
+        }
+    }
+}
