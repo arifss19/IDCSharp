2812cd6357e86611869a7b7f53f323054dbda4b5
Angga Adji Surya <anggas25@live.com>
2020-01-13 18:44:43 +0700

    Simple Login Screen
    Without Database Connection

---
+++ Desktop/loginScreen/loginScreen/Login.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace loginScreen
+{
+    public partial class Login : Form
+    {
+        readonly string Username = "idcsharp";
+        readonly string Password = "12345";
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            if (TxtUsername.Text == Username && TxtPassword.Text == Password)
+            {
+                MessageBox.Show("Kamu berhasil login");
+            }
+            else if (TxtUsername.Text == "" || TxtPassword.Text == "")
+            {
+                MessageBox.Show("Kamu belum mengisi form login");
+            }
+            else
+            {
+                MessageBox.Show("Username atau Password Salah ");
+            }
+        }
+
+        
+    }
+}
